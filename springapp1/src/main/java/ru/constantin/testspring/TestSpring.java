7ed9d575ae8a954662e830e3ca45fc57d955f59e
Konstantin Ovsyannikov <ovsyannikov-kv@yandex.ru>
2020-08-04 01:22:49 +0300

    second commit

--- springapp1/src/main/java/ru/constantin/testspring/TestSpring.java
+++ springapp1/src/main/java/ru/constantin/testspring/TestSpring.java
@@ -9,8 +9,17 @@ public class TestSpring {
         );
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        firstMusicPlayer.playMusic();
+        secondMusicPlayer.playMusic();
+        System.out.println(firstMusicPlayer == secondMusicPlayer);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(13);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
 
 //        System.out.println("Player name = " + musicPlayer.getName());
 //        System.out.println("Player volume = " + musicPlayer.getVolume());

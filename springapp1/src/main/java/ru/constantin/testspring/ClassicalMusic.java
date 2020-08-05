fcf4195ae8231d174f27f79d96e21e4e92489a82
Konstantin Ovsyannikov <ovsyannikov-kv@yandex.ru>
2020-08-05 03:01:08 +0300

    third commit

--- springapp1/src/main/java/ru/constantin/testspring/ClassicalMusic.java
+++ springapp1/src/main/java/ru/constantin/testspring/ClassicalMusic.java
@@ -1,6 +1,20 @@
 package ru.constantin.testspring;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic (){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("do my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("doing my destraction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- springapp1/src/main/java/ru/constantin/testspring/TestSpring.java
+++ springapp1/src/main/java/ru/constantin/testspring/TestSpring.java
@@ -7,19 +7,27 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
+//        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+//        System.out.println(classicalMusic2.getSong());
+
+
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        firstMusicPlayer.playMusic();
-        secondMusicPlayer.playMusic();
-        System.out.println(firstMusicPlayer == secondMusicPlayer);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(13);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        firstMusicPlayer.playMusic();
+//        secondMusicPlayer.playMusic();
+//        System.out.println(firstMusicPlayer == secondMusicPlayer);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(13);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
 //        System.out.println("Player name = " + musicPlayer.getName());
 //        System.out.println("Player volume = " + musicPlayer.getVolume());
